c074af4a1e375352f0718fe406215bb39a9713c6
tsteelematc <tsteele@madisoncollege.edu>
2025-07-04 08:43:01 -0500

    Spaces to tabs : - O
    Playing around with my prefs and VS code settings, blah, blah, blah...

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 const http = require(
-    'http'
+	'http'
 );
 
 const server = http.createServer(
-    (req, res) => {
-        res.writeHead(
-            200
-            , { 
-                'Content-Type': 'text/plain' 
-            }
-        );
-        res.end(
+	(req, res) => {
+		res.writeHead(
+			200
+			, {
+				'Content-Type': 'text/plain'
+			}
+		);
+		res.end(
 `Hello, World ! ! !
 
 The time on the server is ${new Date().toLocaleString()}
 
 The D6 rolled a ${Math.floor(Math.random() * 6) + 1}`
-        );
-    }
+		);
+	}
 );
 
 // Get port from .env file, or uses port 8080 in not in .env file...
 const port = process.env.PORT || 8080;
 
 server.listen(
-    port
-    , () => {
-        console.log(
-            `Listening on port ${port}`
-        );
-    }
+	port
+	, () => {
+		console.log(
+			`Listening on port ${port}`
+		);
+	}
 );
