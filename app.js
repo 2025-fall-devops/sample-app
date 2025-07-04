de75f9ba0ce534886bd7500d804f8d8e193cdbb1
tsteelematc <tsteele@madisoncollege.edu>
2025-07-04 09:33:47 -0500

    Messing with prettier settings...
    Can only use `printWidth` to kind of get function arguments one per line : - (
    
    And no leading commas : - O
    
    Maybe it's my problem, i-o-g...

--- app.js
+++ app.js
@@ -1,33 +1,32 @@
-const http = require(
-	'http'
-);
+const http = require('http');
 
-const server = http.createServer(
-	(req, res) => {
-		res.writeHead(
-			200
-			, {
-				'Content-Type': 'text/plain'
-			}
-		);
-		res.end(
-`Hello, World ! ! !
+const server =
+	http.createServer(
+		(req, res) => {
+			res.writeHead(200, {
+				'Content-Type':
+					'text/plain',
+			});
+			res.end(
+				`Hello, World ! ! !
 
 The time on the server is ${new Date().toLocaleString()}
 
-The D6 rolled a ${Math.floor(Math.random() * 6) + 1}`
-		);
-	}
-);
+The D6 rolled a ${
+					Math.floor(
+						Math.random() * 6,
+					) + 1
+				}`,
+			);
+		},
+	);
 
 // Get port from .env file, or uses port 8080 if not in .env file...
-const port = process.env.PORT || 8080;
+const port =
+	process.env.PORT || 8080;
 
-server.listen(
-	port
-	, () => {
-		console.log(
-			`Listening on port ${port}`
-		);
-	}
-);
+server.listen(port, () => {
+	console.log(
+		`Listening on port ${port}`,
+	);
+});
