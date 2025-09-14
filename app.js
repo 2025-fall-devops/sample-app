fb288337c1808a44e9ab11f5d1aab9ed9387495c
ccittadino <ccittadino@madisoncollege.edu>
2025-09-14 16:08:25 -0500

    Added date/time output.

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World!\n');
+  res.end(`Hello, World! ! ! !
+
+    The time on the server is ${new Date().toLocaleString()}`);
 });
 
 const port = process.env.PORT || 8080;
