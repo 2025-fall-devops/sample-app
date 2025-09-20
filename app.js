03b7b0dfe12550aac0249e2e5addba08f2e297c7
Awilliams35 <awilliams35@madisoncollege.edu>
2025-09-20 11:01:09 -0500

    Add  Random number for diceroll using ${Math.floor(Math.random() * 20)} (Expression)

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const http = require(
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end(
-`Hello, World!
-The time on the server is ${new Date().toLocaleString()}`
+`
+Hello, World!
+The time on the server is ${new Date().toLocaleString()}
+
+The D6 rolled a ${Math.floor(Math.random() * 20)}`
 
   );          
 }
