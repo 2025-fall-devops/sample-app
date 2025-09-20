87e668a8dfd69a653df23f3d1e4fbcffc91a8d0e
Awilliams35 <awilliams35@madisoncollege.edu>
2025-09-20 10:33:35 -0500

    Added ${new Date().toLocaleString()} variable to the sting for DATE

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const http = require(
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World!\n');          
-});
+  res.end(
+`Hello, World!
+The time on the server is ${new Date().toLocaleString()}`
+
+  );          
+}
+);
 
 const port = process.env.PORT || 8080; 
 server.listen(port,() => {
