8f64219b7a13e4c942fa00483db9e28888bc22e5
jegyes <jegyes@madisoncollege.edu>
2025-09-22 02:34:38 -0500

    corrected app; first commit had dropped lines

--- app.js
+++ app.js
@@ -14,3 +14,13 @@ const server = http.createServer(
         );
     }
 );
+
+const port = process.env.PORT || 8080;
+server.listen(
+    port
+    , () => {
+        console.log(
+            `listening on port ${port}`
+        );
+    }
+);
