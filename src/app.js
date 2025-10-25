80dfab6c61584fd32aced0caed9b202683b55ff2
awilliams35 <awilliams35@madisoncollege.edu>
2025-10-25 13:48:35 +0000

    sync up

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const http = require(
+    'http'
+);
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end(
+`
+Hello, World!
+The time on the server is ${new Date().toLocaleString()}
+
+The D6 rolled a ${Math.floor(Math.random() * 20)}`
+
+  );          
+}
+);
+
+const port = process.env.PORT || 8080; 
+server.listen(port,() => {
+  console.log(`Listening on port ${port}`);
+});
+
+// Symlinks ./sample-app/src/app.js & ./sample-app/ansible/ro les/sample-app/files/app.js
