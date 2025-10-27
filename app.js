ae475dcca818ee6669b983b77554e068caf0a3e1
jegyes <jegyes@madisoncollege.edu>
2025-10-26 20:30:26 -0500

    week 3 app files

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ The D20 rolled a ${Math.floor(Math.random() * 20) + 1 }`
     }
 );
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 server.listen(
     port
     , () => {
