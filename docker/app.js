766283f62d76d5c24faa184d7c63a86dcbb4e799
ccittadino-matc <ccittadino@madisoncollege.edu>
2025-12-15 22:30:23 -0600

    small change to test rolling update on K8s

--- docker/app.js
+++ docker/app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end(`Hello, World! ! ! !
+  res.end(`FUNdamentals of DevOps ! ! !
 
     The time on the server is ${new Date().toLocaleString()}
     
