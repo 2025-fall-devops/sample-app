bb90147636e31715d85bc779c88ededce39c9e85
ccittadino-matc <ccittadino@madisoncollege.edu>
2025-12-15 18:03:28 -0600

    Changed my pre-existing D20 roll to a D12.

--- packer/app.js
+++ packer/app.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
 
     The time on the server is ${new Date().toLocaleString()}
     
-    The D20 rolled a ${Math.floor(Math.random()*20)+1}`);
+    The D12 rolled a ${Math.floor(Math.random()*12)+1}`);
 });
 
 const port = process.env.PORT || 8080;
